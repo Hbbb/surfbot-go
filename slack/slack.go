810095af24836323e2d84458a982fafac5ba2d2e
Harrison <hborges005@gmail.com>
2017-11-15 17:15:54 -0800

    write slack package

--- slack/slack.go
+++ slack/slack.go
@@ -1,19 +1,119 @@
 package slack
 
 import (
-	"os"
 	"bytes"
-	"net/http"
 	"encoding/json"
-	"github.com/joho/godotenv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/hbbb/surfbot/surfline"
+	"github.com/joho/godotenv"
 )
 
+// Slack API Format
+// {
+// 		 "title": "SurfBot"
+//     "attachments": [
+//         {
+//             "color": "#36a64f",
+//             "pretext": "Surf Reports 11/15/2017",
+//             "title": "South San Diego",
+//             "title_link": "https://surfline.com/",
+//             "text": "3-4 ft. Occasional 5 ft. on standout sets",
+//             "fields": [
+//                 {
+//                     "title": "Surf Max",
+//                     "value": "5",
+//                     "short": true
+//                 },
+// 				                {
+//                     "title": "Surf Min",
+//                     "value": "2",
+//                     "short": true
+//                 }
+//             ]
+//         }
+//     ]
+// }
+
+type message struct {
+	Title       string
+	Attachments []attachment `json:"attachments"`
+}
+
+type attachment struct {
+	Color       string       `json:"color"`
+	Title       string       `json:"pretext"`
+	Link        string       `json:"title_link"`
+	Headline    string       `json:"text"`
+	SurfHeights []surfHeight `json:"fields"`
+}
+
+type surfHeight struct {
+	Title string `json:"title"`
+	Value int    `json:"value"`
+	Short bool   `json:"short"`
+}
+
 // SendMessage takes in a list of surf reports and sends them as a slack message
-func SendMessage(reports []surfline.Report) {
+func SendMessage(surfReports []surfline.Report) {
 	godotenv.Load()
 	slackURL := os.Getenv("SLACK_URL")
+	message := buildMessage(surfReports)
 
-	payload, _ := json.Marshal(reports)
-	http.Post(slackURL, "application/json", bytes.NewBuffer(payload))
+	payload, _ := json.Marshal(message)
+	resp, err := http.Post(slackURL, "application/json", bytes.NewBuffer(payload))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+}
+
+func buildMessage(reports []surfline.Report) attachment {
+	message := message{Title: "Surf Report"}
+	attachments := buildAttachments(reports)
+	message.Attachments = attachments
+
+	return attachment{}
+}
+
+func buildAttachments(reports []surfline.Report) []attachment {
+	attachments := []attachment{}
+
+	for _, report := range reports {
+		attachment := attachment{
+			Title:       report.SpotName,
+			Color:       "#679AB0",
+			Link:        fmt.Sprintf("https://new.surfline.com/"),
+			Headline:    report.Surf.Text(),
+			SurfHeights: buildFields(report),
+		}
+
+		attachments = append(attachments, attachment)
+	}
+
+	return attachments
+}
+
+func buildFields(r surfline.Report) []surfHeight {
+	var fields []surfHeight
+
+	maxHeight := surfHeight{
+		Title: "Max Height",
+		Value: r.Surf.Max(),
+		Short: true}
+
+	minHeight := surfHeight{
+		Title: "Min Height",
+		Value: r.Surf.Min(),
+		Short: true}
+
+	fields = append(fields, maxHeight)
+	fields = append(fields, minHeight)
+
+	return fields
 }
--- surfline/surfline.go
+++ surfline/surfline.go
@@ -1,4 +1,4 @@
-package main
+package surfline
 
 import (
 	"encoding/json"
@@ -8,21 +8,51 @@ import (
 	"net/http"
 )
 
-const (
-	surfineBaseURL      = "http://api.surfline.com/v1/forecasts/"
-	surflineQueryParams = "?resources=analysis&days=1&getAllSpots=false&units=e&usenearshore=true&interpolate=true&showOptimal=false"
+var (
+	queryString     = "resources=analysis&days=1&getAllSpots=false&units=e&usenearshore=true&interpolate=true&showOptimal=false"
+	surflineSpotIds = []string{"2953", "2144", "131699"}
 )
 
-var surflineSpotIds = []string{"2953", "2144", "131699"}
+// Reports is a list of reports of reports
+type Reports []Report
 
-func main() {
-	reports := GetReports()
-	fmt.Println("Surf Report", reports)
+// Report represents the subset of the raw API response that we care about
+type Report struct {
+	ID        string   `json:"id"`
+	Latitude  string   `json:"lat"`
+	Longitude string   `json:"lon"`
+	SpotName  string   `json:"name"`
+	Surf      analysis `json:"Analysis"`
+	MetaData  meta     `json:"_metadata"`
+}
+
+// analysis contains the specific surf report details
+type analysis struct {
+	Description []string `json:"generalText"`
+	HeightText  []string `json:"surfRange"`
+	MinHeight   []int    `json:"surfMin"`
+	MaxHeight   []int    `json:"surfMax"`
+}
+
+func (a analysis) Text() string {
+	return a.Description[0]
+}
+
+func (a analysis) Max() int {
+	return a.MaxHeight[0]
+}
+
+func (a analysis) Min() int {
+	return a.MinHeight[0]
+}
+
+type meta struct {
+	CreatedAt string `json:"dateCreated"`
 }
 
 // GetReports fetches all surf reports in the spot id list
 func GetReports() []Report {
-	reports := []Report{}
+	reports := Reports{}
 
 	for _, id := range surflineSpotIds {
 		report, err := fetchSurfReport(id)
@@ -40,7 +70,7 @@ func GetReports() []Report {
 
 func fetchSurfReport(id string) (Report, error) {
 	report := Report{}
-	url := surfineBaseURL + id + surflineQueryParams
+	url := fmt.Sprintf("http://api.surfline.com/v1/forecasts/%s?%s", id, queryString)
 	res, err := http.Get(url)
 
 	if err != nil {
@@ -60,26 +90,4 @@ func fetchSurfReport(id string) (Report, error) {
 	defer res.Body.Close()
 
 	return report, nil
-}
-
-// analysis contains the specific surf report details
-type analysis struct {
-	Description []string `json:"generalText"`
-	HeightText  []string `json:"surfRange"`
-	SurfMin     []int `json:"surfMin"`
-	SurfMax     []int `json:"surfMax"`
-}
-
-type meta struct {
-	CreatedAt string `json:"dateCreated"`
-}
-
-// Report represents the subset of the raw API response that we care about
-type Report struct {
-	ID        string   `json:"id"`
-	Latitude  string   `json:"lat"`
-	Longitude string   `json:"lon"`
-	SpotName  string   `json:"name"`
-	Surf      analysis `json:"Analysis"`
-	// MetaData  meta     `json:"_metadata"`
 }
